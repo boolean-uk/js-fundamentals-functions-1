30a797474852557a7df8f46af21fe31cd863ac75
Joel Watkins <eck087@yahoo.co.uk>
2022-09-15 11:41:01 +0100

    all questions done

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,11 @@
 // 2      | 3
 //
 // TODO: write code below
+function addBy1(num) {
+  return ++num
+}
+
+addBy1(1)
 
 // 2. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -18,9 +23,17 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function greetWithSmile(name) {
+  const upCasedChar = name[0].toUpperCase()
+  const restOfName = name.slice(1)
+
+  return `Hi, ${upCasedChar + restOfName} :)`
+}
+
+greetWithSmile('bob')
 
 // TODO: change undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined to increment a number (the first TODO)
-  b: undefined // 2. change undefined to be the name of the function you defined to say hi (the second TODO)
+  a: addBy1, // 1. change undefined to be the name of the function you defined to increment a number (the first TODO)
+  b: greetWithSmile // 2. change undefined to be the name of the function you defined to say hi (the second TODO)
 }

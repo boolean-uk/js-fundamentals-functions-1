f04ec19306589f62c21b5582f89cf0a536be7323
Luciano Simoni <lucianosimonipersonal@gmail.com>
2022-09-20 02:36:29 +0100

    Done

--- src/creating-functions-multiple-args.js
+++ src/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function firstQuestion(lower, upper) {
+  const arr = []
+  for (let i = lower; i < upper + 1; i++) {
+    arr.push(i)
+  }
+  return arr
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,9 +28,16 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function secondQuestion(message, times) {
+  message = message.toUpperCase()
+  for (let i = 0; i < times; i++) {
+    message = message + '!'
+  }
+  return message
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: firstQuestion, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: secondQuestion // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function firstQuestion(num) {
+  return (num += 1)
+}
 
 // 2. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -18,9 +21,14 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function secondQuestion(name) {
+  const firstChar = name.charAt(0).toUpperCase()
+  console.log(firstChar)
+  return `Hi, ${firstChar + name.substring(1)} :)`
+}
 
 // TODO: change undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined to increment a number (the first TODO)
-  b: undefined // 2. change undefined to be the name of the function you defined to say hi (the second TODO)
+  a: firstQuestion, // 1. change undefined to be the name of the function you defined to increment a number (the first TODO)
+  b: secondQuestion // 2. change undefined to be the name of the function you defined to say hi (the second TODO)
 }

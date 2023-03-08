0de8f10eb856fb4577afdc731d2ecdf0c07cd132
gid-ctrl <gideonusenbor@yahoo.co.uk>
2023-03-08 16:43:38 +0000

    completed creating - functions

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -8,6 +8,12 @@
 //
 // TODO: write code below
 
+function addOne(num1) {
+  return num1 + 1
+}
+let add1 = addOne(6)
+console.log(add1)
+
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -19,6 +25,19 @@
 
 // TODO: write code below
 
+function firstCapital(input) {
+  let firstChar = input.charAt(0)
+  console.log(firstChar.toUpperCase())
+  return
+}
+firstCapital('bonjour')
+
+// function addCapital(caps1) {
+//   return caps1.toUpperCase
+// }
+// let capital1 =
+// console.log
+
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,6 +48,13 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+function Names1(input) {
+  let firstChar = input.charAt(0).toUpperCase() + input.slice(1)
+  console.log('Hi,', firstChar, ':)')
+  return
+}
+Names1('edward')
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -42,6 +68,18 @@
 //
 // TODO: write code below
 
+function numstrings(numarray) {
+  let counter = 0
+  for (let i = 0; i < numarray.length; i++) {
+    if (typeof numarray[i] === 'string') {
+      counter += 1
+    }
+  }
+  console.log(counter)
+  return
+}
+numstrings(['john', 'steven'])
+
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -53,11 +91,21 @@
 //
 // TODO: write code below
 
+function amazingEd(object) {
+  if (object.edward) {
+    return object
+  } else {
+    object.edward = 'amazing'
+    return object
+  }
+}
+console.log(amazingEd({}))
+
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: addOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: firstCapital, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: Names1, // etc
+  d: numstrings,
+  e: amazingEd
 }

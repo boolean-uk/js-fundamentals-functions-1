0c2c25738cfd164a6c572930bd2756dbafded7ee
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-03-08 19:23:15 +0100

    Passed standard criteria and extension criteria

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,10 @@
 // 2      | 3
 //
 // TODO: write code below
+function incrementNumber(num) {
+  return (num += 1)
+}
+console.log(incrementNumber(3))
 
 // 2. Define a function that capitalises any string
 
@@ -16,6 +20,11 @@
 // hello | Hello
 // world | World
 // Hello | Hello
+
+function captialiseString(str) {
+  return str[0].toUpperCase() + str.slice(1)
+}
+console.log(captialiseString('hello'))
 
 // TODO: write code below
 
@@ -30,6 +39,12 @@
 //
 // TODO: write code below
 
+function greeting(name) {
+  const captialisedName = name.charAt(0).toUpperCase() + name.slice(1)
+  return `Hi, ${captialisedName} :)`
+}
+console.log(greeting('Gabriel'))
+
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -42,6 +57,18 @@
 //
 // TODO: write code below
 
+const testArray = ['1', 1, '1', 1, 1, 1, '1', '1', 1, '1', 1, 1, 1] // 5 strings
+function countStrings(arr) {
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      count += 1
+    }
+  }
+  return count
+}
+console.log(countStrings(testArray))
+
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -52,12 +79,20 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+const testObject = { aiyana: 'fantastic' }
+function addEdward(obj) {
+  if (!obj.edward) {
+    obj.edward = 'amazing'
+  }
+  return obj
+}
+console.log(addEdward(testObject))
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementNumber, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: captialiseString, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: greeting, // etc
+  d: countStrings,
+  e: addEdward
 }

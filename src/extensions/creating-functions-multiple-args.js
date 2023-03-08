f4603bc630f3d53d735da54bbb38c9e7144eceb4
Gabriel Rosengren <gabbe.rosengren@gmail.com>
2023-03-08 21:41:57 +0100

    Refactored extension q#3

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -53,40 +53,57 @@ console.log(makeVeryClear('Name', 5))
 // '23:50', 30  | '00:20'
 // TODO: write code below
 
+// function convertTime(str, num) {
+//   let hour
+//   let minutes
+//   if (str[1] !== ':') {
+//     hour = Number(str[0] + str[1])
+//     minutes = Number(str[3] + str[4])
+//   } else {
+//     hour = Number(str[0])
+//     minutes = Number(str[2] + str[3])
+//   }
+//   console.log(hour, minutes)
+//   for (let i = 0; i < num; i++) {
+//     // will run 52 times
+//     minutes += 1
+//     if (minutes === 60) {
+//       minutes = 0
+//       hour += 1
+//       console.log('minutes reached 60')
+//       if (hour === 24) {
+//         hour = 0
+//         console.log('hours reached 24')
+//       }
+//     }
+//   }
+//   // 0:33
+//   if (hour === 0) {
+//     hour = hour.toString() + '0'
+//   }
+//   if (minutes < 10) {
+//     minutes = '0' + minutes.toString()
+//   }
+//   return `${hour}:${minutes}`
+// }
+
+// Revised Version
 function convertTime(str, num) {
-  let hour
-  let minutes
-  if (str[1] !== ':') {
-    hour = Number(str[0] + str[1])
-    minutes = Number(str[3] + str[4])
-  } else {
-    hour = Number(str[0])
-    minutes = Number(str[2] + str[3])
+  const minutes = Number(str.slice(str.indexOf(':') + 1))
+  const hours = Number(str.slice(0, str.indexOf(':')))
+  const totalMinutes = hours * 60 + minutes + num
+  let newMinutes = totalMinutes % 60
+  let newHours = Math.floor(totalMinutes / 60) % 24
+
+  if (newHours === 0) {
+    newHours = newHours.toString() + '0'
   }
-  console.log(hour, minutes)
-  for (let i = 0; i < num; i++) {
-    // will run 52 times
-    minutes += 1
-    if (minutes === 60) {
-      minutes = 0
-      hour += 1
-      console.log('minutes reached 60')
-      if (hour === 24) {
-        hour = 0
-        console.log('hours reached 24')
-      }
-    }
+  if (newMinutes < 10) {
+    newMinutes = '0' + newMinutes.toString()
   }
-  // 0:33
-  if (hour === 0) {
-    hour = hour.toString() + '0'
-  }
-  if (minutes < 10) {
-    minutes = '0' + minutes.toString()
-  }
-  return `${hour}:${minutes}`
+  return `${newHours}:${newMinutes}`
 }
-console.log(convertTime('23:41', 52))
+console.log(convertTime('23:41', 20))
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
   a: count, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)

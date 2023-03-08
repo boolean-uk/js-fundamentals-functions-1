cf4e89e83055bde0839583758c00c9e2c3dee15d
desokan <thefdmm@gmail.com>
2023-03-08 19:37:16 +0000

    rasheed-adesokan solution to exercise js-fundamentals-functions-1

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,41 @@
 // 2      | 3
 //
 // TODO: write code below
+// FUNCTION DECLARATION
+// let numIncrement = 0;
+// function numIncrease(num) {
+//   numIncrement = num + 1;
+//   return numIncrement;
+// }
+
+// const result = numIncrease(11);
+// console.log(result);
+
+// FUNCTION EXPRESSION
+// let numIncrement = 0;
+// const numIncrease = function (num) {
+//   numIncrement = num + 1;
+//   return numIncrement;
+// };
+
+// const result = numIncrease(11);
+// console.log(result);
+
+// ARROW FUNCTION
+// let numIncrement = 0;
+// function a(num) {
+//   numIncrement = num + 1;
+//   return numIncrement;
+// };
+
+let numIncrement = 0
+const a = (num) => {
+  numIncrement = num + 1
+  return numIncrement
+}
+
+const increaseResult = a(1)
+console.log(increaseResult)
 
 // 2. Define a function that capitalises any string
 
@@ -19,6 +54,15 @@
 
 // TODO: write code below
 
+function b(text) {
+  const firstCharLow = text.charAt(0)
+  const firstCharCap = text.replace(firstCharLow, firstCharLow.toUpperCase())
+  return firstCharCap
+}
+
+const result = b('adesokan')
+console.log(result)
+
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,6 +73,16 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+function c(text) {
+  const firstChar = text.charAt(0)
+  const nameCap = text.replace(firstChar, firstChar.toUpperCase())
+  const greetings = `Hi, ${nameCap} :)`
+  return greetings
+}
+
+const nameOutput = c('Rasheed Adesokan')
+console.log(nameOutput)
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -42,6 +96,20 @@
 //
 // TODO: write code below
 
+function d(array) {
+  const stringArr = []
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index]
+    if (typeof element === 'string') {
+      stringArr.push(element)
+    }
+  }
+  return stringArr.length
+}
+
+const arrayList = [1, 'edward', 11, 45, 'Rasheed']
+console.log(d(arrayList))
+
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -52,12 +120,24 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function e(objData) {
+  const name = 'edward'
+  if (name in objData) {
+    return objData
+  } else {
+    objData[name] = 'amazing'
+    return objData
+  }
+}
+
+const checkObject = { aiyana: 'fantastic' }
+console.log(e(checkObject))
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: a, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: b, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: c, // etc
+  d: d,
+  e: e
 }

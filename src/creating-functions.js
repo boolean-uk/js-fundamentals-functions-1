5d466d8c6215351066d890317031d4b7de653351
Sara Beazley <sarajbeazley@gmail.com>
2023-03-08 20:24:30 -0800

    Completed function exercises

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,10 @@
 // 2      | 3
 //
 // TODO: write code below
+function addsOne(num) {
+  return num + 1
+}
+console.log(addsOne(1))
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +22,11 @@
 // Hello | Hello
 
 // TODO: write code below
+function addsCap(string) {
+  const capitalString = string[0].toUpperCase()
+  return capitalString + string.slice(1)
+}
+console.log(addsCap('Hello'))
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +38,11 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function addSmiley(string) {
+  const name = string[0].toUpperCase()
+  return 'Hi, ' + name + string.slice(1) + ' :)'
+}
+console.log(addSmiley('edward'))
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -42,6 +56,17 @@
 //
 // TODO: write code below
 
+const array = ['Edward', 'Aiyana', 4, 6, 'Jason', 10]
+function stringCount() {
+  let nonStringCount = 0
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string') {
+      nonStringCount = nonStringCount + 1
+    }
+  }
+  return nonStringCount
+}
+console.log(stringCount())
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -53,11 +78,29 @@
 //
 // TODO: write code below
 
+let object = { 
+
+}
+ console.log(object)
+ function changeObject() {
+   let properties = Object.keys(object)
+   for (let i = 0; i <= properties.length; i++) {
+   if ((properties[i] === 'edward') && (object[properties[i]] === 'amazing')) {
+           object.edward = 'amazing'
+         } else if ((properties[i] !== 'edward') && (object[properties[i]] !== 'amazing')) {
+           object.edward = 'amazing'
+     }
+   }
+   return object
+ }
+ 
+ console.log(changeObject())
+
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: addsOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: addsCap, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: addSmiley, // etc
+  d: stringCount,
+  e: changeObject
 }

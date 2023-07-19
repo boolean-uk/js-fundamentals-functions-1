2c4440121712daf7bc0b7d208c530568185225de
jakublaba <jakub.maciej.laba@gmail.com>
2023-07-19 13:11:02 +0200

    Complete core exercises

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,7 @@
 // 2      | 3
 //
 // TODO: write code below
+const increment = (n) => n + 1
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +19,7 @@
 // Hello | Hello
 
 // TODO: write code below
+const capitalize = (s) => s[0].toUpperCase() + s.substring(1)
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +31,7 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+const greet = (s) => `Hi, ${capitalize(s)} :)`
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +44,8 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+const countStrings = (arr) =>
+  arr.filter((e) => typeof e === 'string' || e instanceof String).length
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +57,18 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+const edwardize = (o) => {
+  if (o.edward === undefined) {
+    o.edward = 'amazing'
+  }
+  return o
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalize, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: greet, // etc
+  d: countStrings,
+  e: edwardize
 }

000024908aa4d210336113d420ed565f8bd53acd
ks46 <konastaf46@gmail.com>
2023-08-10 20:07:54 +0300

    Submit extention answers

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function rangeToArray(lower, upper) {
+  const arr = []
+  for (let num = lower; num <= upper; ++num) {
+    arr.push(num)
+  }
+  return arr
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,13 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function strToUpperCaseWithExclamations(str, num) {
+  let exclamations = ''
+  for (let i = 0; i < num; ++i) {
+    exclamations += '!'
+  }
+  return str.toUpperCase() + exclamations
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +47,30 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function addMinutesToTime(time, minutes) {
+  const splittedTime = time.split(':')
+  // add minutes
+  let mins = parseInt(splittedTime[1]) + minutes
+  // add hours
+  let hours = (parseInt(splittedTime[0]) + Math.trunc(mins / 60)) % 24
+  // remove any hours inside minutes result
+  mins %= 60
+  // create minutes result string
+  if (mins < 10) {
+    mins = '0' + mins.toString()
+  } else {
+    mins = mins.toString()
+  }
+  // create hours result string
+  hours = hours === 0 ? '00' : hours.toString()
+
+  const result = hours + ':' + mins
+  return result
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: rangeToArray, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: strToUpperCaseWithExclamations, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addMinutesToTime // etc
 }

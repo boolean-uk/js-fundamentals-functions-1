fc30c7f0de3873fc176f435421b6ff003127a622
Petros Moustakis <petrosmoukas16@gmail.com>
2023-08-10 18:54:53 +0300

    function1 exercises

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // 1. Define a function that takes a number and increments it by 1.
 //
 // Example Input and Output:
@@ -7,15 +8,20 @@
 // 2      | 3
 //
 // TODO: write code below
-
+// eslint-disable-next-line no-unused-vars
+function increaseByOne(number) {
+  return number + 1
+}
 // 2. Define a function that capitalises any string
-
 // Example Input and Output:
 //
 // Input | Output
 // hello | Hello
 // world | World
 // Hello | Hello
+function capitalise(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
 
 // TODO: write code below
 
@@ -29,8 +35,12 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function smileyName(name) {
+  return 'Hi, ' + capitalise(name) + ' :)'
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
+
 //
 // Example Input and Output:
 //
@@ -41,6 +51,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function stringsCount(arr) {
+  let count = 0
+  for (const item of arr) {
+    if (typeof item === 'string') {
+      count++
+    }
+  }
+  return count
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +71,17 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function edward(obj) {
+  // eslint-disable-next-line no-prototype-builtins
+  if (!obj.hasOwnProperty('edward')) obj.edward = 'amazing'
+  return obj
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increaseByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: smileyName, // etc
+  d: stringsCount,
+  e: edward
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // 1. define a function that takes two numbers: lower and upper.
 // The function should return an array containing all the numbers between
 // lower and upper, including lower and upper.
@@ -9,6 +10,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function lowerAndUpper(lower, upper) {
+  const arr = []
+  for (let i = lower; i <= upper; i++) {
+    arr.push(i)
+  }
+  return arr
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +29,13 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function exclamationMark(str, num) {
+  let count = ''
+  for (let i = 0; i < num; i++) {
+    count += '!'
+  }
+  return str.toUpperCase() + count
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -35,8 +50,9 @@
 // TODO: write code below
 
 // TODO: change the exported value to be the name of the function you defined
+
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: lowerAndUpper, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: exclamationMark, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
   c: undefined // etc
 }

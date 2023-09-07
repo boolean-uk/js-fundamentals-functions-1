1a2ed512188b32ed71ba453082e01283546b0686
Lukas Dembicki <lad_777@hotmail.com>
2023-09-07 17:06:54 +0100

    finished creating-functions

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -10,6 +10,8 @@
 function incrementNumber(num1) {
   return num1 + 1
 }
+
+// CHECK ME WITH THIS.......
 console.log(incrementNumber(1))
 
 // 2. Define a function that capitalises any string
@@ -27,6 +29,8 @@ function capitalize(string) {
   return string && string[0].toUpperCase() + string.slice(1)
 }
 const saying = 'hello'
+
+// CHECK ME WITH THIS.......
 console.log(capitalize(saying))
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
@@ -40,6 +44,13 @@ console.log(capitalize(saying))
 //
 // TODO: write code below
 
+function smiley(name) {
+  const capitalizedname = capitalize(name)
+  return `Hi, ${capitalizedname} :)`
+}
+// CHECK ME WITH THIS....
+console.log(smiley('edward'))
+
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -51,6 +62,17 @@ console.log(capitalize(saying))
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function stringfinder(array) {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string') {
+      count++
+    }
+  }
+  return count
+}
+
+console.log(stringfinder(['lukas', 'ayo', 1, 3, 'person']))
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -63,12 +85,27 @@ console.log(capitalize(saying))
 //
 // TODO: write code below
 
+function propertyEdward(obj) {
+  const keys = Object.keys(obj)
+  if (!keys.includes('edward')) {
+    obj.edward = 'amazing'
+  }
+  return obj
+}
+
+const awesomepeople = {
+  aiyana: 'fantastic'
+}
+
+console.log(propertyEdward(awesomepeople))
+console.log(Object.keys(awesomepeople))
+
 // TODO: change each undefined below to be the name of the functions you defined
 
 module.exports = {
   a: incrementNumber, // 1. change undefined to be the name of the function you defined for the first TODO
   b: capitalize, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  c: smiley, // etc
+  d: stringfinder,
+  e: propertyEdward
 }

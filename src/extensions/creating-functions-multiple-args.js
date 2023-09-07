b0be7bab1b3cba3ca2d1cdb06390a5a3b516c00f
TomEastwood <t.eastwood99@gmail.com>
2023-09-07 17:52:16 +0100

    extensions complete

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,14 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function range(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
+console.log(range(1, 3))
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +29,14 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function shout(string, number) {
+  let shout = string.toUpperCase()
+  for (let i = 0; i < number; i++) {
+    shout += '!'
+  }
+  return shout
+}
+console.log(shout('disaster', 5))
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +49,26 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
-
+function time(string, number) {
+  const time = string.split(':')
+  const hours = parseInt(time[0])
+  const minutes = parseInt(time[1])
+  let newMinutes = minutes + number
+  let newHours = hours + Math.floor(newMinutes / 60)
+  newMinutes = newMinutes % 60
+  newHours = newHours % 24
+  if (newHours === 0) {
+    newHours = '0' + newHours
+  }
+  if (newMinutes < 10) {
+    newMinutes = '0' + newMinutes
+  }
+  return newHours + ':' + newMinutes
+}
+console.log(time('23:50', 30))
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: range, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: shout, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: time // etc
 }

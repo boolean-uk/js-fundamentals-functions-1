7d91fe7a59e78b90ee41324b11f30e2e79004c7c
usama ibrahim <usamaibrahimusman@gmail.com>
2023-09-30 17:17:55 +0100

    completed exercise

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,12 @@
 // 2      | 3
 //
 // TODO: write code below
+function AddNumberByOne(input) {
+  return input + 1;
+}
+
+const Addnumber = AddNumberByOne(7)
+console.log(Addnumber)
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +24,9 @@
 // Hello | Hello
 
 // TODO: write code below
+function CapitalizedString(TurnToString) {
+  return TurnToString.charAt(0).toUpperCase() + TurnToString.slice(1).toLowerCase();
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +38,10 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function Smiley(PersonName) {
+  const capitalized = CapitalizedString(PersonName);
+  return `Hi, ${capitalized} :)`;
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +54,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function ReturnNumberOfArray(Array_Of_Data) {
+  let count = 0;
+  for (const item of Array_Of_Data) {
+    if (typeof item === 'string') {
+      count++;
+    }
+  }
+  return count;
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +74,23 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+const empty_object = {}
+
+function AddObject(obj) {
+  if (!obj.hasOwnProperty('edward')) {
+    obj.edward = 'amazing';
+  }
+  return obj;
+}
+
+
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
-}
+  a: AddNumberByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: CapitalizedString, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: Smiley,                // etc
+  d: ReturnNumberOfArray,
+  e: AddObject,
+};
+

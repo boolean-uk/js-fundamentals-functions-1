e3cc10b81ee0cc3255a8633c73ef7f2b94809788
Najem Hamo <najem.hamo@gmail.com>
2024-02-15 14:32:14 +0100

    Core & Extension

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function rangeOfNumbers(lower, upper) {
+  const numbers = []
+  for (let i = lower; i <= upper; i++) {
+    numbers.push(i)
+  }
+  return numbers
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,9 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function shout(string, number) {
+  return string.toUpperCase() + '!'.repeat(number)
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +43,18 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function addTime(time, minutes) {
+  const [hour, minute] = time.split(':').map(Number)
+  const newMinute = (minute + minutes) % 60
+  const newHour = (hour + Math.floor((minute + minutes) / 60)) % 24
+  return `${newHour.toString().padStart(2, '0')}:${newMinute
+    .toString()
+    .padStart(2, '0')}`
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: rangeOfNumbers, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: shout, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addTime // etc
 }

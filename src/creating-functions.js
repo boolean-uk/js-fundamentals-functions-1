a42560136ec422c7f768d618ed6b995173b8eafb
André Sturesson <andre.m.sturesson@gmail.com>
2024-02-15 13:36:49 +0100

    Core + Extensions

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -1,63 +1,36 @@
-// 1. Define a function that takes a number and increments it by 1.
-//
-// Example Input and Output:
-//
-// Input  | Output
-// 1      | 2
-// 2      | 3
-//
-// TODO: write code below
+function addOne(num) {
+  return num + 1
+}
 
-// 2. Define a function that capitalises any string
+function capitaliseWord(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
 
-// Example Input and Output:
-//
-// Input | Output
-// hello | Hello
-// world | World
-// Hello | Hello
+function addSmiley(name) {
+  return 'Hi, ' + capitaliseWord(name) + ' :)'
+}
 
-// TODO: write code below
+function countStrings(arr) {
+  let count = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      count++
+    }
+  }
+  return count
+}
 
-// 3. Define a function that takes any person's name and returns it with a smiley :)!
-// Remember to make the name capitalized!
-//
-// Example Input and Output:
-//
-// Input  | Output
-// edward | Hi, Edward :)
-// Aiyana | Hi, Aiyana :)
-//
-// TODO: write code below
+function addEdwardProperty(obj) {
+  if (!obj.hasOwnProperty('edward')) {
+    obj.edward = 'amazing'
+  }
+  return obj
+}
 
-// 4. Define a function that takes an array of data and returns how many strings are in the array.
-//
-// Example Input and Output:
-//
-// Input                | Output
-// ['edward']           | 1
-// ['edward', 'aiyana'] | 2
-// [1]                  | 0
-// [1, 'edward']        | 1
-//
-// TODO: write code below
-
-// 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
-//
-// Example Input and Output:
-//
-// Input                    | Output
-// {}                       | { edward: 'amazing' }
-// { aiyana: 'fantastic' }  | { aiyana: 'fantastic', edward: 'amazing' }
-// { edward: 'fabulous'}    | { edward: 'fabulous'}
-//
-// TODO: write code below
-
-// TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: addOne,
+  b: capitaliseWord,
+  c: addSmiley,
+  d: countStrings,
+  e: addEdwardProperty
 }

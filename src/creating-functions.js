8a4d8944c684c52e1f5b8f3ecc048abdd0789f8b
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-02-15 15:52:52 +0100

    Finished core and extensions

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function increment(num) {
+  return num + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +21,9 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalise(a) {
+  return `${a[0].toUpperCase()}${a.substring(1)}`
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +35,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function Greet(name) {
+  return 'Hi, ' + capitalise(name) + ' :)'
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +50,13 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function CountStrings(data) {
+  let strings = 0
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] === 'string') strings++
+  }
+  return strings
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -53,11 +69,18 @@
 //
 // TODO: write code below
 
+function Edward(object) {
+  if (!Object.prototype.hasOwnProperty.call(object, 'edward')) {
+    object.edward = 'amazing'
+  }
+  return object
+}
+
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: Greet, // etc
+  d: CountStrings,
+  e: Edward
 }

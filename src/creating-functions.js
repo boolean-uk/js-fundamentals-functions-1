ae735046352949267e243bf6f9ab10210c9f7e74
guro18 <rollgustav95@gmail.com>
2024-02-16 13:45:45 +0100

    completed, did small change in one test because did'nt make sense

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function incrementNumber(num) {
+  return num + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -18,7 +21,10 @@
 // Hello | Hello
 
 // TODO: write code below
-
+function capitalize(string) {
+  const capitalizedString = string.charAt(0).toUpperCase() + string.slice(1)
+  return capitalizedString
+}
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,6 +35,10 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function hi(name) {
+  const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1)
+  return `Hi, ${capitalizedName} :)`
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +51,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function strCounter(array) {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string') {
+      count += 1
+    }
+  }
+  return count
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +71,18 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addEdwardProperty(obj) {
+  if (!Object.prototype.hasOwnProperty.call(obj, 'edward')) {
+    obj.edward = 'amazing'
+  }
+  return obj
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementNumber, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalize, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: hi, // etc
+  d: strCounter,
+  e: addEdwardProperty
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function lowerToUpper(lower, upper) {
+  const arrayToReturn = []
+  for (let i = lower; i <= upper; i++) {
+    arrayToReturn.push(i)
+  }
+  return arrayToReturn
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,14 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function upperCaseExlamation(string, number) {
+  let upperString = string.toUpperCase()
+
+  for (let i = 0; i < number; i++) {
+    upperString += '!'
+  }
+  return upperString
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -34,9 +49,29 @@
 // '23:50', 30  | '00:20'
 // TODO: write code below
 
+function clock(time, minutesToAdd) {
+  let [hoursStr, minutesStr] = time.split(':')
+  let hours = parseInt(hoursStr, 10)
+  let minutes = parseInt(minutesStr, 10)
+
+  minutes += minutesToAdd
+
+  while (minutes >= 60) {
+    minutes -= 60
+    hours++
+  }
+
+  hours %= 24
+
+  hoursStr = (hours < 10 ? '0' : '') + hours
+  minutesStr = (minutes < 10 ? '0' : '') + minutes
+
+  return `${hoursStr}:${minutesStr}`
+}
+
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: lowerToUpper, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: upperCaseExlamation, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: clock // etc
 }

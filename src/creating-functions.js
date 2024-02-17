3a25532058fe812b21c772d2890f196d1d2d1597
Caroline Berg <caroline.berg@no.experis.com>
2024-02-17 15:13:34 +0100

    Test is not consistent on timeformat. changed to double digits, exercise complete

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -8,6 +8,9 @@
 //
 // TODO: write code below
 
+function incrementByOne(num) {
+  return num + 1
+}
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -18,8 +21,13 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitaliseString(input) {
+  const letter = input.charAt(0).toUpperCase()
+  return letter + input.slice(1)
+}
+console.log(capitaliseString('string'))
 
-// 3. Define a function that takes any person's name and returns it with a smiley :)!
+// 3. Define a function that takes any person's name and returns it with a smiley :)
 // Remember to make the name capitalized!
 //
 // Example Input and Output:
@@ -29,7 +37,10 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
-
+function sayHi(name) {
+  return 'Hi, ' + capitaliseString(name) + ' :)'
+}
+console.log(sayHi('elisa'))
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -41,6 +52,16 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function stringCounter(arr) {
+  let counter = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === typeof 'string') {
+      counter++
+    }
+  }
+  return counter
+}
+console.log('stringCounter(["dueu", 1, "2"]):', stringCounter(['dueu', 1, '2']))
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +73,20 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function objectEditor(input) {
+  if (input.edward === undefined) {
+    input.edward = 'amazing'
+  }
+  return input
+}
+console.log(objectEditor({ edward: 'okay' }))
+console.log(objectEditor({ a: 'well..' }))
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitaliseString, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: sayHi, // etc
+  d: stringCounter,
+  e: objectEditor
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -10,6 +10,15 @@
 //
 // TODO: write code below
 
+function makeArray(lower, upper) {
+  const array = []
+  for (let i = 0; i <= upper - lower; i++) {
+    array.push(lower + i)
+  }
+  return array
+}
+console.log('makeArray(5, 10)', makeArray(5, 10))
+
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,7 +30,14 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
-
+function AddExclamationMarks(inputString, numOfMarks) {
+  let marks = ''
+  for (let i = numOfMarks; i > 0; i--) {
+    marks += '!'
+  }
+  return inputString.toUpperCase() + marks
+}
+console.log('AddMarks:', AddExclamationMarks('hello', 6))
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
 //
@@ -33,10 +49,32 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
-
+function AddMintuesToTime(time, minutes) {
+  const timesplit = time.split(':')
+  const mseconds =
+    parseInt(timesplit[0]) * 60 * 60 * 1000 +
+    parseInt(timesplit[1]) * 60 * 1000 +
+    minutes * 60 * 1000
+  const newTime = new Date(mseconds)
+  timesplit[0] = newTime.getHours() - 1
+  if (timesplit[0] < 10) {
+    timesplit[0].toString()
+    timesplit[0] = '0' + timesplit[0]
+  } else {
+    timesplit[0].toString()
+  }
+  timesplit[1] = newTime.getMinutes().toString()
+  return timesplit[1] < 10
+    ? `${timesplit[0]}:0${timesplit[1]}`
+    : `${timesplit[0]}:${timesplit[1]}`
+}
+console.log(
+  'AddMintuesToTime("14:02" , 75)- result:',
+  AddMintuesToTime('14:02', 75)
+)
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: makeArray, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: AddExclamationMarks, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: AddMintuesToTime // etc
 }

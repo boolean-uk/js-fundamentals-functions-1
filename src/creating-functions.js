50c70e2c6368ef8194d10bb6a4deb60dd6156651
Tølløv Aresvik <tollov.aresvik@no.experis.com>
2024-02-19 16:00:16 +0100

    Tests: Passed

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,9 +7,10 @@
 // 2      | 3
 //
 // TODO: write code below
-
+function increaseByOne(num) {
+  return num + 1
+}
 // 2. Define a function that capitalises any string
-
 // Example Input and Output:
 //
 // Input | Output
@@ -18,6 +19,12 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalise(str) {
+  return str
+    .split(' ')
+    .map((word) => word[0].toUpperCase() + word.substring(1).toLowerCase())
+    .join(' ')
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,7 +36,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
-
+function addSmiley(str) {
+  return 'Hi, ' + capitalise(str) + ' :)'
+}
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -41,7 +50,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
-
+function lengthOfArray(arr) {
+  let result = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'string') {
+      result++
+    }
+  }
+  return result
+}
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -52,12 +69,15 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
-
+function addAmazingEdward(obj) {
+  obj.edward = obj.edward === undefined ? 'amazing' : obj.edward
+  return obj
+}
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increaseByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: addSmiley, // etc
+  d: lengthOfArray,
+  e: addAmazingEdward
 }

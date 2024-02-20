3fc655503ddf63702994e96031f2b5a882187bad
Stian Andreas Ytterstad Nordvik <stian.andreas.ytterstad.nordvik@no.experis.com>
2024-02-20 10:43:47 +0100

    Stian Andreas Ytterstad Nordvik C#3

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,14 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function task1(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
+task1(1, 5)
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +29,13 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function task2(word, amount) {
+  let ex = ''
+  for (let i = 0; i < amount; i++) {
+    ex += '!'
+  }
+  return word.toUpperCase() + ex
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +48,28 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function task3(str, addMin) {
+  const [hoursStr, minutesStr] = str.split(':')
+  let hours = parseInt(hoursStr)
+  let minutes = parseInt(minutesStr)
+
+  minutes += addMin
+
+  hours += Math.floor(minutes / 60)
+  minutes %= 60
+  hours %= 24
+
+  const hourFormatted =
+    hours < 10 ? String(hours) : String(hours).padStart(2, '0')
+
+  const time = `${hourFormatted}:${String(minutes).padStart(2, '0')}`
+
+  return time
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: task1, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: task2, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: task3 // etc
 }

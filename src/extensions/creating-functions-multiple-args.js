c03d4038edd37149f60c46274f27e4bcd7c91f80
Eddy1108 <edvardh@hotmail.no>
2024-02-20 11:44:34 +0100

    Core + Extension -1

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,16 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function fill(lower, upper){
+  if (lower > upper) return null
+
+  let arr = []
+  for (let index = lower; index <= upper; index++) {
+    arr.push(index)
+  }
+
+  return arr
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +31,13 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function scream(message, screamNum){
+  let scr = "!"
+  for (let index = 1; index < screamNum; index++) {
+    scr = scr + "!"
+  }
+  return message.toUpperCase() + scr
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +50,13 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function TimeAdd(time, toAdd){
+  //No time to do
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: fill, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: scream, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: TimeAdd // etc
 }

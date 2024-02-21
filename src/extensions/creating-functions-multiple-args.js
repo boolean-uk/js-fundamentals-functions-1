d26c4aafb624230badd0f89062b97b8ea6482bcb
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-02-21 19:07:15 +0000

    samisaeed

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -10,6 +10,12 @@
 //
 // TODO: write code below
 
+function num(lower,upper){
+  //let arr = [1,2,3,4,5]
+  for (let i = 0; i < arr.length;){
+
+  }
+}
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,6 +27,11 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function capitalise(word,num){
+  let result = word.toUpperCase()
+  let exclamation = '!'
+  console.log(`${word}${exclamation.repeat(num)}`)
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -36,7 +47,7 @@
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: num, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: capitalise, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
   c: undefined // etc
 }

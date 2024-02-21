2242d1a2badb923f7e1a1ea7308d868d16ad728e
Knut S Rostad <knut.rostad@no.experis.com>
2024-02-21 15:25:16 +0100

    All done

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,7 +7,10 @@
 // 2      | 3
 //
 // TODO: write code below
-
+function incrementNum(num) {
+  num++
+  return num
+}
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -18,7 +21,11 @@
 // Hello | Hello
 
 // TODO: write code below
-
+function capitaliseStr(stringToFix) {
+  const firstLetter = stringToFix.charAt(0).toUpperCase()
+  const fixedString = firstLetter + stringToFix.slice(1)
+  return fixedString
+}
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,7 +36,12 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
-
+function returnSmiley(name) {
+  const firstLetter = name.charAt(0).toUpperCase()
+  const fixedName = firstLetter + name.slice(1)
+  const returnSentence = `Hi, ${fixedName} :)`
+  return returnSentence
+}
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -41,7 +53,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
-
+function countArray(array) {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'string') {
+      count++
+    }
+  }
+  return count
+}
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -52,12 +72,17 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
-
+function addEddy(object) {
+  if (object.edward === undefined) {
+    object.edward = 'amazing'
+  }
+  return object
+}
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementNum, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitaliseStr, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: returnSmiley, // etc
+  d: countArray,
+  e: addEddy
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,7 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
-
+function takeTwoNums(lower, upper) {
+  const array = []
+  for (let i = lower; i <= upper; i++) {
+    array.push(i)
+  }
+  return array
+}
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,7 +27,14 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
-
+function createString(str, num) {
+  let excl = ''
+  let newString = str.toUpperCase()
+  for (let i = 1; i <= num; i++) {
+    excl += '!'
+  }
+  return (newString += excl)
+}
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
 //
@@ -33,10 +46,29 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function fixTime(str, num) {
+  const [hourStr, minutesStr] = str.split(':')
+  let hour = parseInt(hourStr)
+  let minutes = parseInt(minutesStr)
 
+  minutes += num
+  minutes += hour * 60
+
+  hour = Math.floor(minutes / 60)
+  minutes = minutes % 60
+  hour %= 24
+
+  if (hour === 0) {
+    hour += '0'
+  } else if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  const returnString = `${hour}:${minutes}`
+  return returnString
+}
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: takeTwoNums, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: createString, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: fixTime // etc
 }

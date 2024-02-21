c8d384d1ce9475646480d45b84741cfa3f886772
Santhia <santhia.shanmukarasan@gmail.com>
2024-02-21 10:18:29 +0100

    All tasks completed

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function incrementNumberByOne(number){
+  return number + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +21,10 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalizeString(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +36,13 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function greetNameWithSmiley(name) {
+  // Capitalize the first letter of the name
+  const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+  // Return the greeting with the capitalized name and a smiley
+  return `Hi, ${capitalized} :)`;
+}
+
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +55,22 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function countStrings(arr) {
+  // Initialize a variable to count the number of strings
+  let count = 0;
+  
+  // Loop through each element in the array
+  for (let element of arr) {
+      // Check if the element is a string
+      if (typeof element === 'string') {
+          // If it's a string, increment the count
+          count++;
+      }
+  }
+  // Return the final count
+  return count;
+}
+
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +82,24 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addEdwardProperty(obj) {
+  // Check if the 'edward' property already exists in the object
+  if (!obj.hasOwnProperty('edward')) {
+      // If it doesn't exist, add it with the default value 'amazing'
+      obj.edward = 'amazing';
+  }
+  
+  // Return the modified object
+  return obj;
+}
+
+
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementNumberByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalizeString, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: greetNameWithSmiley, // etc
+  d: countStrings,
+  e: addEdwardProperty
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,17 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function NumbersInBetween(lower, upper){
+  const result = [];
+  for (let i = lower; i <= upper; i++){
+    result.push(i);
+  }
+  return result
+}
+// Test cases
+console.log(NumbersInBetween(1, 3));   // Output: [1, 2, 3]
+console.log(NumbersInBetween(10, 13)); // Output: [10, 11, 12, 13]
+console.log(NumbersInBetween(-1, 1));  // Output: [-1, 0, 1]
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +32,15 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function makeUpperCaseWithExclamation(str, num) {
+  return str.toUpperCase() + '!'.repeat(num);
+}
+
+// Test cases
+console.log(makeUpperCaseWithExclamation('disaster', 5)); // Output: "DISASTER!!!!!"
+console.log(makeUpperCaseWithExclamation('error', 10));   // Output: "ERROR!!!!!!!!!!"
+
+
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -32,11 +52,39 @@
 // '11:50', 20  | '12:10'
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
+
 // TODO: write code below
+function addTimeInMinutes(time, minutes) {
+  // Split the time string into hours and minutes
+  const [hourStr, minuteStr] = time.split(':');
+  let hour = parseInt(hourStr);
+  let mins = parseInt(minuteStr);
+  
+  // Add the additional minutes
+  let totalMinutes = hour * 60 + mins + minutes;
+  
+  // Calculate the new hours and minutes
+  let newHour = Math.floor(totalMinutes / 60) % 24;
+  let newMin = totalMinutes % 60;
+  
+  // Format the new time
+  let formattedHour = newHour === 0 ? '00' : newHour.toString();
+  let formattedMin = newMin < 10 ? '0' + newMin.toString() : newMin.toString();
+  
+  return `${formattedHour}:${formattedMin}`;
+}
+
+// Test cases
+console.log(addTimeInMinutes('7:50', 4));    // Output: "7:54"
+console.log(addTimeInMinutes('7:50', 72));   // Output: "9:02"
+console.log(addTimeInMinutes('11:50', 20));  // Output: "12:10"
+console.log(addTimeInMinutes('12:50', 120)); // Output: "14:50"
+console.log(addTimeInMinutes('23:50', 30));  // Output: "00:20"
+
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: NumbersInBetween, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: makeUpperCaseWithExclamation, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addTimeInMinutes // etc
 }

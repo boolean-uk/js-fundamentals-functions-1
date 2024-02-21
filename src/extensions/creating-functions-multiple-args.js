3571f4dd059d059ade1b907ae0c9d12aac45ed12
Rafa Lopes <rafalopessecond@gmail.com>
2024-02-21 11:42:16 +0000

    exercises

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function getArrByRange(lower, upper) {
+  const arr = []
+  for (let i = lower; i <= upper; i++) {
+    arr.push(i)
+  }
+  return arr
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,9 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function exclamationInCaps(str, num) {
+  return str.toUpperCase() + '!'.repeat(num)
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +43,33 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function minsToHours(m) {
+  if (m >= 60) {
+    let h = Math.floor(m / 60)
+    let mPercent = ('' + m / 60).split('.')[1],
+      m = (mPercent / 10) * 60
+    return [h, m]
+  } else return m
+}
+
+function addTime(timeStr, mins) {
+  const [timeH, timeM] = timeStr.split(':')
+  const [minsH, m] = minsToHours(mins + timeM)
+  const check24Clock = (h) => (h > 23 ? check24Clock(h - 24) : h)
+  const getDoubleDigits = (n) => {
+    if (('' + n).length < 2) {
+      return '0' + n
+    }
+    return n
+  }
+  const h = check24Clock(timeH + minsH)
+
+  return `${getDoubleDigits(h)}:${getDoubleDigits(m)}`
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: getArrByRange, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: exclamationInCaps, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addTime // etc
 }

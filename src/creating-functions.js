639cf6119785719d72282452039292a164ece339
Andreas Lauvhjell <andreas.lauvhjell@no.experis.com>
2024-02-22 11:35:44 +0100

    C#Ex3 Andreas Lauvhjell

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,7 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
-
+function iterate(num) {
+  return ++num
+}
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -18,7 +20,9 @@
 // Hello | Hello
 
 // TODO: write code below
-
+function capitalise(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,7 +33,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
-
+function greeting(name) {
+  return 'Hi, ' + capitalise(name) + ' :)'
+}
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
 // Example Input and Output:
@@ -41,7 +47,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
-
+function stringNum(arr) {
+  let strAmount = 0
+  arr.forEach((element) => {
+    if (typeof element === 'string' || element instanceof String) {
+      strAmount++
+    }
+  })
+  return strAmount
+}
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -52,12 +66,18 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
-
+function edwardLovesIt(object) {
+  // eslint-disable-next-line no-prototype-builtins
+  if (!object.hasOwnProperty('edward')) {
+    object.edward = 'amazing'
+  }
+  return object
+}
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: iterate, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: greeting, // etc
+  d: stringNum,
+  e: edwardLovesIt
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,7 +9,14 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
-
+function numBetween(min, max) {
+  const arr = [min]
+  for (let i = min + 1; i < max; i++) {
+    arr.push(i)
+  }
+  arr.push(max)
+  return arr
+}
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,7 +28,13 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
-
+function exaggerator(string, num) {
+  string = string.toUpperCase()
+  for (let i = 0; i < num; i++) {
+    string += '!'
+  }
+  return string
+}
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
 //
@@ -33,10 +46,28 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function tickTocker(time, mins) {
+  const separator = time.indexOf(':')
+  let hours = parseInt(time.substring(0, separator))
+  let minutes = parseInt(time.substring(separator + 1))
+
+  hours += Math.floor((minutes + mins) / 60)
+  minutes = (minutes + mins) % 60
+  hours = hours % 24
+
+  if (hours === 0) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+
+  return hours + ':' + minutes
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: numBetween, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: exaggerator, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: tickTocker // etc
 }

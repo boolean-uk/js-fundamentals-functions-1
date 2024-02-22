29bb26f734f15753fc16338fcf8f2849792f71fd
stp <steventphung6@gmail.com>
2024-02-22 08:44:22 +0100

    Steven Tong Phung

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -8,6 +8,10 @@
 //
 // TODO: write code below
 
+function addOne(number) {
+  return number + 1
+}
+
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -18,7 +22,9 @@
 // Hello | Hello
 
 // TODO: write code below
-
+function capitalise(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
 //
@@ -29,6 +35,10 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+
+function hi(name) {
+  return `Hi, ${capitalise(name)} :)`
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +51,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function countStrings(array) {
+  let count = 0
+  for (let index = 0; index < array.length; index++) {
+    if (typeof array[index] === 'string') {
+      count++
+    }
+  }
+  return count
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +71,19 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function edward(object) {
+  if(object.edward === undefined) {
+    object.edward = 'amazing'
+  }
+
+  return object
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: addOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: hi, // etc
+  d: countStrings,
+  e: edward
 }

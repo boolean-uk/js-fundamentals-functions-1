523717e3510d28066e04fd6801c53d96f4452dfe
Alistair Henshaw <alistairhenshaw@gmail.com>
2024-02-22 17:04:04 +0000

    extension complete

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -57,9 +57,63 @@ console.log(shout('hello', 5))
 // '23:50', 30  | '00:20'
 // TODO: write code below
 
+function clock(string, number) {
+  let hours = 0
+  let minutes = 0
+
+  minutes += number
+
+  if (string.length === 5) {
+    hours += parseInt(string.slice(0, 2))
+    minutes += parseInt(string.slice(3))
+  } else if (string.length === 4) {
+    hours += parseInt(string.slice(0, 1))
+    minutes += parseInt(string.slice(2))
+  }
+
+  // console.log(hours)
+  // console.log(minutes)
+
+  while (minutes > 60) {
+    minutes -= 60
+    hours++
+  }
+
+  while (hours >= 24) {
+    hours -= 24
+  }
+
+  // console.log(hours)
+  // console.log(minutes)
+
+  let newTime = ''
+
+  if (hours === 0) {
+    newTime = '00:'
+  } else {
+    newTime = String(hours) + ':'
+  }
+
+  if (minutes >= 10) {
+    newTime += String(minutes)
+  } else {
+    newTime += '0' + String(minutes)
+  }
+
+  return newTime
+}
+
+console.log(clock('1:03', 122))
+
+// practice
+// const word = "70"
+// const test = parseInt(word)
+// console.log(test)
+// console.log(String(test))
+
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: numArray, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: shout, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: clock // etc
 }

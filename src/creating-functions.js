10050551a320e7c9d27a5b841c6f8471df4d9057
Ateeb <ateeb@live.no>
2024-02-23 03:58:56 +0100

    core

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -8,6 +8,9 @@
 //
 // TODO: write code below
 
+function increment(number){
+  return number+1;
+}
 // 2. Define a function that capitalises any string
 
 // Example Input and Output:
@@ -18,6 +21,9 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalises(text){
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +35,10 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function returnSmiley(name) {
+  return 'Hi, ' + capitalises(name) + ' :)';
+}
+
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -42,6 +52,17 @@
 //
 // TODO: write code below
 
+
+function findArrLength(arr) {
+  let stringCount = 0;
+  arr.forEach(element => {
+    if (typeof element === 'string') {
+      stringCount++;
+    }
+  });
+  return stringCount;
+}
+
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
 // Example Input and Output:
@@ -52,12 +73,20 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addProperty(obj) {
+  if (obj.edward === undefined) {
+    obj.edward = 'amazing';
+  }
+  return obj;
+}
+
+
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: increment, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalises, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: returnSmiley, // etc
+  d: findArrLength,
+  e: addProperty
 }

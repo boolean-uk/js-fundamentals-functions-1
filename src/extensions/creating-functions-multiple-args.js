78b6e014c3f2a7c4ce2f99a78977e841b57c7500
homonoviscoding <mama.leye@gmail.com>
2024-02-23 20:36:11 +0000

    the last one was hard and required some thourough research

--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,7 +9,15 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
-
+function twoNumbers(lower, upper) {
+  const numbers = []
+  for (let i = lower; i <= upper; i++) {
+    numbers.push(i)
+  }
+  return numbers
+}
+const array = twoNumbers(6, 18)
+console.log(array)
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -21,7 +29,15 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
-
+function capitalise(str, num) {
+  let result = str.toUpperCase()
+  for (let i = 1; i <= num; i++) {
+    result += '!'
+  }
+  return result
+}
+const toUpper = capitalise('disaster', 5)
+console.log(toUpper)
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
 //
@@ -33,10 +49,27 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function addMinutesToTime(timeString, additionalMinutes) {
+  const [hoursStr, minutesStr] = timeString.split(':')
+  const hours = +hoursStr
+  const minutes = +minutesStr
 
+  const totalMinutes = (hours * 60 + minutes + additionalMinutes) % (24 * 60)
+
+  const newHours = totalMinutes / 60 - ((totalMinutes / 60) % 1)
+  const newMinutes = totalMinutes % 60
+  let newTime
+  if (newHours === 0) {
+    newTime = `00:${String(newMinutes).padStart(2, '0')}`
+  } else {
+    newTime = `${newHours}:${String(newMinutes).padStart(2, '0')}`
+  }
+  return newTime
+}
+console.log(addMinutesToTime('23:50', 30))
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: twoNumbers, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: capitalise, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: addMinutesToTime // etc
 }

524c52fa0f327a171ece138dad9267868adf4f9b
JOANNABUUMA <nabuumaj6@gmail.com>
2024-03-21 08:24:54 +0000

    Solution for fundamental functions 1

--- src/creating-functions.js
+++ src/creating-functions.js
@@ -7,6 +7,9 @@
 // 2      | 3
 //
 // TODO: write code below
+function incrementByOne(number) {
+  return number + 1
+}
 
 // 2. Define a function that capitalises any string
 
@@ -18,6 +21,9 @@
 // Hello | Hello
 
 // TODO: write code below
+function capitalise(input) {
+  return input.charAt(0).toUpperCase() + input.substr(1)
+}
 
 // 3. Define a function that takes any person's name and returns it with a smiley :)!
 // Remember to make the name capitalized!
@@ -29,6 +35,9 @@
 // Aiyana | Hi, Aiyana :)
 //
 // TODO: write code below
+function appendSmiley(name) {
+  return `Hi, ${name.charAt(0).toUpperCase() + name.substr(1)} :)`
+}
 
 // 4. Define a function that takes an array of data and returns how many strings are in the array.
 //
@@ -41,6 +50,15 @@
 // [1, 'edward']        | 1
 //
 // TODO: write code below
+function countStrings(data) {
+  let count = 0
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i] === 'string') {
+      count++
+    }
+  }
+  return count
+}
 
 // 5. Define a function that takes an object and adds a property 'edward' to the object if it doesn't yet exist with a default value of 'amazing'
 //
@@ -52,12 +70,15 @@
 // { edward: 'fabulous'}    | { edward: 'fabulous'}
 //
 // TODO: write code below
+function addProperty(input) {
+  return { edward: 'amazing', ...input }
+}
 
 // TODO: change each undefined below to be the name of the functions you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function you defined for the first TODO
-  b: undefined, // 2. change undefined to be the name of the function you defined for the second TODO)
-  c: undefined, // etc
-  d: undefined,
-  e: undefined
+  a: incrementByOne, // 1. change undefined to be the name of the function you defined for the first TODO
+  b: capitalise, // 2. change undefined to be the name of the function you defined for the second TODO)
+  c: appendSmiley, // etc
+  d: countStrings,
+  e: addProperty
 }
--- src/extensions/creating-functions-multiple-args.js
+++ src/extensions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function functionOne(lower, upper) {
+  const answer = []
+  for (let i = lower; i <= upper; i++) {
+    answer[answer.length] = i
+  }
+  return answer
+}
 
 // 2. define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,6 +28,14 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function functionTwo(string, number) {
+  let i = 0
+  while (i < number) {
+    string = string + '!'
+    i++
+  }
+  return string.toUpperCase()
+}
 
 // 3. define a function that takes two arguments: a string and a number.
 // The function should return the new time in 24hr time after adding the additional time in minutes.
@@ -33,10 +48,20 @@
 // '12:50', 120 | '14:50'
 // '23:50', 30  | '00:20'
 // TODO: write code below
+function functionThree(time, addMinutes) {
+  const tokens = time.split(':')
+  let minutes = Number(tokens[1]) + addMinutes
+  let hours = Number(tokens[0]) + Math.floor(minutes / 60)
+
+  hours = hours >= 24 ? hours - 24 : hours
+  minutes = minutes % 60
+
+  return `${hours < 1 ? '00' : hours}:${minutes < 10 ? '0' + minutes : minutes}`
+}
 
 // TODO: change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
-  c: undefined // etc
+  a: functionOne, // 1. change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: functionTwo, // 2. change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  c: functionThree // etc
 }
